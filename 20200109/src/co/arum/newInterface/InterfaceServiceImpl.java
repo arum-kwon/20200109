017af377b266b4cf06c1de4ee671c54645bb258a
User <User@YD01-15>
2020-01-10 17:47:28 +0900

    20/01/10

--- 20200109/src/co/arum/newInterface/InterfaceServiceImpl.java
+++ 20200109/src/co/arum/newInterface/InterfaceServiceImpl.java
@@ -3,6 +3,7 @@ package co.arum.newInterface;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class InterfaceServiceImpl implements InterfaceService {
@@ -10,11 +11,25 @@ public class InterfaceServiceImpl implements InterfaceService {
 	public PreparedStatement psmt;
 	public ResultSet rs;
 
+	private String ALL_SELET= "SELECT * FROM countries";
 	private String SELET= "SELECT * FROM countries WHERE country_id = ?";
 	
 	@Override
 	public List<?> allSelect() throws SQLException {
-		return null;
+		CountryDao dao = new CountryDao();
+		CountryDto dto;
+		List<CountryDto> list = new ArrayList<CountryDto>();
+		
+		psmt = dao.conn.prepareStatement(ALL_SELET);
+		rs = psmt.executeQuery();
+		while(rs.next()) {
+			dto = new CountryDto();
+			dto.setCountry_id(rs.getString("country_id"));
+			dto.setCountry_name(rs.getString("country_name"));
+			dto.setRegion_id(rs.getInt("region_id"));
+			list.add(dto);
+		}
+		return list;
 	}
 
 	@Override
--- 20200109/src/co/arum/newInterface/MainApp.java
+++ 20200109/src/co/arum/newInterface/MainApp.java
@@ -1,10 +1,33 @@
 package co.arum.newInterface;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainApp {
 
 	public static void main(String[] args) {
+		allSelectPrint();
+	}
+
+	public static void allSelectPrint() {
+		InterfaceServiceImpl service = new InterfaceServiceImpl();
+		List<?> list = new ArrayList<CountryDto>();
+		
+		try {
+			list = service.allSelect();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		for(Object dto : list) {
+			CountryDto countDto = (CountryDto)dto;
+			System.out.print(countDto.getCountry_id() + " ");
+			System.out.print(countDto.getCountry_name() + " ");
+			System.out.print(countDto.getRegion_id() + "\n");
+		}
+	}
+	public static void select() {
 		InterfaceServiceImpl service = new InterfaceServiceImpl();
 		CountryDto dto = new CountryDto();
 		
@@ -18,5 +41,4 @@ public class MainApp {
 		System.out.print(dto.getCountry_name() + " | ");
 		System.out.print(dto.getRegion_id() + "\n");
 	}
-
 }
